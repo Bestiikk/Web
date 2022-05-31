cf00ba689b5a56dae6e1f299314969e090e6b28b
Bestik <vitalijzabolotnij8@gmail.com>
2022-05-31 03:24:04 +0300

    Lab8

---
+++ Lab8/script.js
@@ -0,0 +1,20 @@
+(function (global) {
+    var script = {};
+    script.names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+    console.log("Масив: " + script.names);
+    console.log("Cпосіб селекціонування імен: за першою буквою 'j'");
+    script.names.forEach(i =>
+      (i.toLowerCase()[0] == 'j' ? SpeakGoodBye.speak(i) : SpeakHello.speak(i))
+    )
+    console.log(" " + "\n");
+  
+    script.names = ["Vitalik", "Lera", "John", "Andrii", "Misha", "Cena", "Lesnar", "Chris", "Ronaldo", "Messi", "Randy"];
+    console.log("Масив імен : " + script.names);
+    console.log("Cпосіб селекціонування імен: за четвертою буквою 'a'");
+    script.names.forEach(i =>
+      (i.toLowerCase()[3] == 'a' ? SpeakGoodBye.speak(i) : SpeakHello.speak(i))
+    )
+    console.log(" " + "\n");
+  
+    global.script = script
+  })(window)
